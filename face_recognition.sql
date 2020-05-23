e134f3aa493548705e183fff13fea3f62fdccc22
WonderQ <gordon_517@163.com>
2020-05-23 15:37:57 +0800

    更新数据库

--- face_recognition.sql
+++ face_recognition.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 80017
+ Source Server Version : 80019
  Source Host           : localhost:3306
- Source Schema         : face_recognition
+ Source Schema         : face_rec
 
  Target Server Type    : MySQL
- Target Server Version : 80017
+ Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 16/05/2020 09:03:27
+ Date: 23/05/2020 15:35:53
 */
 
 SET NAMES utf8mb4;
@@ -22,32 +22,26 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
-  `aid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `pwd` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `aid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `pwd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`aid`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- ----------------------------
--- Records of account
--- ----------------------------
-BEGIN;
-INSERT INTO `account` VALUES (1, 'yqp', '123');
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for records
 -- ----------------------------
 DROP TABLE IF EXISTS `records`;
 CREATE TABLE `records` (
-  `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `aid` int(11) DEFAULT NULL,
-  `appear` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `disappear` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `last_time` int(16) DEFAULT NULL,
+  `rid` int NOT NULL AUTO_INCREMENT,
+  `aid` int DEFAULT NULL,
+  `object_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `appear` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `disappear` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_time` int DEFAULT NULL,
   PRIMARY KEY (`rid`),
   KEY `fk_records_account` (`aid`),
   CONSTRAINT `fk_records_account` FOREIGN KEY (`aid`) REFERENCES `account` (`aid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
