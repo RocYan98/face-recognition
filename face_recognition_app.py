6645892b41d4602137b9b73f0dbc5fafcd0fe499
Yan <rocyan98@gmail.com>
2020-05-26 16:39:50 +0800

    repair bug

--- face_recognition_app.py
+++ face_recognition_app.py
@@ -25,6 +25,15 @@ def sql_conn(sql):
     return ret
 
 
+def get_file_name():
+    L = []
+    for root, dirs, files in os.walk(dataset):
+        for file in files:
+            if os.path.splitext(file)[1] == '.png':
+                L.append(os.path.join(os.path.splitext(file)[0]))
+    return L
+
+
 class User:
     id = None
     object_name = None
@@ -32,7 +41,7 @@ class User:
 
 
 user = User
-dataset = "/Users/Yan/Projects/PycharmProjects/face_rec/dataset"
+dataset = "/Users/Yan/Projects/PycharmProjects/face_rec/dataset/"
 
 
 class LoginPage:
@@ -143,15 +152,6 @@ def change_password():
     Button(top, text='确认修改', width=10, command=change).place(x=130, y=200)
 
 
-def file_name():
-    L = []
-    for root, dirs, files in os.walk(dataset):
-        for file in files:
-            if os.path.splitext(file)[1] == '.png':
-                L.append(os.path.join(os.path.splitext(file)[0]))
-    return L
-
-
 class MainPage:
     def __init__(self):
 
@@ -240,7 +240,7 @@ class MainPage:
         scr1['command'] = lb.yview
         scr1.grid(row=1, column=1, sticky=E + N + S)
 
-        for item in file_name():
+        for item in get_file_name():
             lb.insert("end", item)
 
             def choose_one():
@@ -250,25 +250,6 @@ class MainPage:
         Button(top, text="确定", command=choose_one).grid(row=2, columnspan=2, pady=10, ipadx=20)
 
         top.mainloop()
-
-
-def set_name():
-    def set():
-        user.object_name = object_name.get()
-        tkinter.messagebox.showerror('成功', '设置姓名成功')
-        top.destroy()
-
-    top = Toplevel()
-    top.title('修改密码')
-    top.maxsize(350, 250)
-    top.geometry('350x250')
-    Label(top, text='欢迎使用人脸识别系统', width=40, height=2,
-          bg='#56764C').grid(row=0, sticky=W + E)
-
-    object_name = StringVar()
-    Label(top, text='输入姓名: ').place(x=45, y=60)
-    Entry(top, textvariable=object_name).place(x=110, y=60)
-    Button(top, text='确认修改', width=10, command=set).place(x=130, y=200)
 
 
 class UpdatePic:
@@ -286,7 +267,7 @@ class UpdatePic:
         self.canva = Canvas(self.frame, width=200, height=200, bg="gray")
         self.canva.grid(row=1, column=1)
         tkinter.Button(self.frame, text="选择文件", width=10, command=self.choose_file).grid(row=2, column=1, pady=10)
-        tkinter.Button(self.frame, text="设置姓名", width=10, command=set_name).grid(row=3, column=1, pady=10)
+        tkinter.Button(self.frame, text="设置姓名", width=10, command=self.set_name).grid(row=3, column=1, pady=10)
         tkinter.Button(self.frame, text="确定", width=10, command=self.upload).grid(row=4, column=1, pady=10)
 
         self.root.mainloop()
@@ -294,47 +275,40 @@ class UpdatePic:
     def choose_file(self):
         filename = tkinter.filedialog.askopenfilename(title='选择文件')
         img = cv.imread(filename)
-        self.Showimage(img, self.canva, "fill")
+        self.Showimage(img, self.canva)
         self.filename = filename
 
     def upload(self):
         img = Image.open(self.filename)
-        img.save(dataset + user.object_name + '.png')
-        tkinter.messagebox.showinfo('', '上传成功!')
-        self.root.destroy()
+        try:
+            img.save(dataset + user.object_name + '.png')
+            tkinter.messagebox.showinfo('', '上传成功!')
+            self.root.destroy()
+        except TypeError:
+            tkinter.messagebox.showerror('', '请设置姓名')
 
-    def Showimage(self, imgCV_in, canva, layout="null"):
-        """
-        Showimage()是一个用于在tkinter的canvas控件中显示OpenCV图像的函数。
-        使用前需要先导入库
-        import cv2 as cv
-        from PIL import Image,ImageTktkinter
-        并注意由于响应函数的需要，本函数定义了一个全局变量 imgTK，请不要在其他地方使用这个变量名!
-        参数：
-        imgCV_in：待显示的OpenCV图像变量
-        canva：用于显示的tkinter canvas画布变量
-        layout：显示的格式。可选项为：
-            "fill"：图像自动适应画布大小，并完全填充，可能会造成画面拉伸
-            "fit"：根据画布大小，在不拉伸图像的情况下最大程度显示图像，可能会造成边缘空白
-            给定其他参数或者不给参数将按原图像大小显示，可能会显示不全或者留空
-        """
-        global imgTK
+    def set_name(self):
+        def set():
+            user.object_name = object_name.get()
+            tkinter.messagebox.showinfo('成功', '设置姓名成功')
+            top.destroy()
+
+        top = Toplevel()
+        top.title('修改密码')
+        top.maxsize(350, 250)
+        top.geometry('350x250')
+        Label(top, text='欢迎使用人脸识别系统', width=40, height=2,
+              bg='#56764C').grid(row=0, sticky=W + E)
+
+        object_name = StringVar()
+        Label(top, text='输入姓名: ').place(x=45, y=60)
+        Entry(top, textvariable=object_name).place(x=110, y=60)
+        Button(top, text='确认修改', width=10, command=set).place(x=130, y=200)
+
+    def Showimage(self, imgCV_in, canva):
         canvawidth = int(canva.winfo_reqwidth())
         canvaheight = int(canva.winfo_reqheight())
-        sp = imgCV_in.shape
-        cvheight = sp[0]  # height(rows) of image
-        cvwidth = sp[1]  # width(colums) of image
-        if layout == "fill":
-            imgCV = cv.resize(imgCV_in, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
-        elif layout == "fit":
-            if float(cvwidth / cvheight) > float(canvawidth / canvaheight):
-                imgCV = cv.resize(imgCV_in, (canvawidth, int(canvawidth * cvheight / cvwidth)),
-                                  interpolation=cv.INTER_AREA)
-            else:
-                imgCV = cv.resize(imgCV_in, (int(canvaheight * cvwidth / cvheight), canvaheight),
-                                  interpolation=cv.INTER_AREA)
-        else:
-            imgCV = imgCV_in
+        imgCV = cv.resize(imgCV_in, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
         imgCV2 = cv.cvtColor(imgCV, cv.COLOR_BGR2RGBA)  # 转换颜色从BGR到RGBA
         current_image = Image.fromarray(imgCV2)  # 将图像转换成Image对象
         imgTK = ImageTk.PhotoImage(image=current_image)  # 将image对象转换为imageTK对象
