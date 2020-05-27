07213f9535c6f331516af546d45f6e1dc6d300f3
Yan <rocyan98@gmail.com>
2020-05-27 15:45:41 +0800

    repair bug

--- face_recognition_app.py
+++ face_recognition_app.py
@@ -194,11 +194,6 @@ class MainPage:
         UpdatePic()
 
     def open_records(self):
-        def claer_history():
-            sql = """truncate table records"""
-            sql_conn(sql)
-            top.destroy()
-
         top = Toplevel()
         top.title('记录')
         top.maxsize(600, 600)
@@ -223,8 +218,6 @@ class MainPage:
         scrollbar = Scrollbar(top)
         scrollbar.grid(row=1, column=1, sticky=NS)
         scrollbar.config(command=tree.yview)
-        # Button(top, text="清除记录", width=10, command=claer_history).grid(row=2, column=0, pady=10, padx=10)
-        # top.mainloop()
 
     def choose_person(self):
         global choose_one
@@ -275,7 +268,7 @@ class UpdatePic:
     def choose_file(self):
         filename = tkinter.filedialog.askopenfilename(title='选择文件')
         img = cv.imread(filename)
-        self.Showimage(img, self.canva)
+        self.Showimage(img, self.canva, "fill")
         self.filename = filename
 
     def upload(self):
@@ -290,11 +283,10 @@ class UpdatePic:
     def set_name(self):
         def set():
             user.object_name = object_name.get()
-            tkinter.messagebox.showinfo('成功', '设置姓名成功')
             top.destroy()
 
         top = Toplevel()
-        top.title('修改密码')
+        top.title('设置姓名')
         top.maxsize(350, 250)
         top.geometry('350x250')
         Label(top, text='欢迎使用人脸识别系统', width=40, height=2,
@@ -303,12 +295,40 @@ class UpdatePic:
         object_name = StringVar()
         Label(top, text='输入姓名: ').place(x=45, y=60)
         Entry(top, textvariable=object_name).place(x=110, y=60)
-        Button(top, text='确认修改', width=10, command=set).place(x=130, y=200)
+        Button(top, text='确认', width=10, command=set).place(x=130, y=200)
 
-    def Showimage(self, imgCV_in, canva):
+    def Showimage(self, imgCV_in, canva, layout="null"):
+        """
+        Showimage()是一个用于在tkinter的canvas控件中显示OpenCV图像的函数。
+        使用前需要先导入库
+        import cv2 as cv
+        from PIL import Image,ImageTktkinter
+        并注意由于响应函数的需要，本函数定义了一个全局变量 imgTK，请不要在其他地方使用这个变量名!
+        参数：
+        imgCV_in：待显示的OpenCV图像变量
+        canva：用于显示的tkinter canvas画布变量
+        layout：显示的格式。可选项为：
+            "fill"：图像自动适应画布大小，并完全填充，可能会造成画面拉伸
+            "fit"：根据画布大小，在不拉伸图像的情况下最大程度显示图像，可能会造成边缘空白
+            给定其他参数或者不给参数将按原图像大小显示，可能会显示不全或者留空
+        """
+        global imgTK
         canvawidth = int(canva.winfo_reqwidth())
         canvaheight = int(canva.winfo_reqheight())
-        imgCV = cv.resize(imgCV_in, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
+        sp = imgCV_in.shape
+        cvheight = sp[0]  # height(rows) of image
+        cvwidth = sp[1]  # width(colums) of image
+        if layout == "fill":
+            imgCV = cv.resize(imgCV_in, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
+        elif layout == "fit":
+            if float(cvwidth / cvheight) > float(canvawidth / canvaheight):
+                imgCV = cv.resize(imgCV_in, (canvawidth, int(canvawidth * cvheight / cvwidth)),
+                                  interpolation=cv.INTER_AREA)
+            else:
+                imgCV = cv.resize(imgCV_in, (int(canvaheight * cvwidth / cvheight), canvaheight),
+                                  interpolation=cv.INTER_AREA)
+        else:
+            imgCV = imgCV_in
         imgCV2 = cv.cvtColor(imgCV, cv.COLOR_BGR2RGBA)  # 转换颜色从BGR到RGBA
         current_image = Image.fromarray(imgCV2)  # 将图像转换成Image对象
         imgTK = ImageTk.PhotoImage(image=current_image)  # 将image对象转换为imageTK对象
